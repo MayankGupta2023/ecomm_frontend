48be042a063027f448d45fcd3b6d0c6ec909ad8d
Mayank Gupta <mayankgupta20231@gmail.com>
2024-12-02 19:27:04 +0530

    Update proxy URL in package.json and remove unused CSRF token fetching code in user.js

--- src/mocks/user.js
+++ src/mocks/user.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-// const getCSRFToken = async () => {
-//     const response = await axios.get('http://127.0.0.1:8000/api/csrf/');
-//     axios.defaults.headers.common['X-CSRFToken'] = response.data.csrfToken;
-// };
 
 class UserAPI {
 
